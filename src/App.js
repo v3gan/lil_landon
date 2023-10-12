b150a21f4ffbca86d90354a7c5cd6fbbb6c222fc
marcus <marcmonk@gmail.com>
2023-10-12 07:50:59 -0400

    uses npm ci in amplify.yaml and adds fun text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Today!
+          Learn React Today With Marcus!
         </a>
       </header>
     </div>
